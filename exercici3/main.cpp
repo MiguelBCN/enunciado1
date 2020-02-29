c636d398b5482c6e0ceef5b8fdbfa6f8732ca335
MiguelDesktop <huayllasmiguel@gmail.com>
2020-02-29 23:21:39 +0100

    Update 1,2,3,4

--- exercici3/main.cpp
+++ exercici3/main.cpp
@@ -13,45 +13,48 @@
 
 
 using namespace std;
+int opcion;
+int contador = 0;
+int area;
+vector<string> arr_opcion = {"Salir", "Introducir circulo"};
 
+void introducirCirculo(int &contador) {
+    contador++;
+    cout << "Circulo numero " << contador << "\n";
+
+    cout << "Radi?\n";
+    cin>>area;
+
+    //Creamos el objeto 
+    Circle circulo(area);
+
+    cout << "El area del circulo es " << circulo.getArea() << "\n";
+}
 
 int main() {
 
-    int opcion;
-    int area;
-    vector<string> arr_opcion = {"Salir", "Introducir circulo"};
-
-
-
     do {
-        cout << "Hola "  << " que quieres hacer?\n";
+        cout << "Hola " << " que quieres hacer?\n";
         for (int i = 0; i < arr_opcion.size(); i++) {
-            cout << i+1 << ". " << arr_opcion[i] << "\n";
+            cout << i + 1 << ". " << arr_opcion[i] << "\n";
         }
 
         cin>> opcion;
-        
 
         switch (opcion) {
             case 1:
-                opcion=0;
-            case 2:
-                
-                cout<<"Radi?\n";
-                cin>>area;
-                
-                //Creamos el objeto 
-                Circle circulo(area);
-                try{
-                cout<<"El area del circulo es "<<circulo.getArea()<<"\n";}
-                catch(const std::invalid_argument& e){
-                cout<<"Error numerico\n";}
+                opcion = 0;
                 break;
-       
+            case 2:
+                introducirCirculo(contador);
+                break;
+
         }
 
     } while (opcion != 0);
     cout << "***FIN***\n";
 
     return 0;
+
+
 }
--- exercici4/main.cpp
+++ exercici4/main.cpp
@@ -6,50 +6,57 @@
  * Created on 28 de febrero de 2020, 22:28
  */
 
-
+//USAR OVERLOADING? PARA LAS DOS FORMAS
 #include<iostream>
 #include <vector>
 #include "Circle.h"
 #include "Ellipse.h"
 
 using namespace std;
+int opcion;
+int contador = 0;
+int area;
+int numCirculos=0;
+int numElipses=0;
+vector<string> arr_opcion = {"Salir", "Introducir circulo"};
 
+void agregarFigura(int &contador) {
+    contador++;
+    cout << "Circulo numero " << contador << "\n";
+
+    cout << "Radi?\n";
+    cin>>area;
+
+    //Creamos el objeto 
+    Circle circulo(area);
+
+    cout << "El area del circulo es " << circulo.getArea() << "\n";
+}
 
 int main() {
 
-    int opcion;
-    int area;
-    vector<string> arr_opcion = {"Salir", "Introducir circulo"};
-
-
-
     do {
-        cout << "Hola "  << " que quieres hacer?\n";
+        cout << "Hola " << " que quieres hacer?\n";
         for (int i = 0; i < arr_opcion.size(); i++) {
-            cout << i+1 << ". " << arr_opcion[i] << "\n";
+            cout << i + 1 << ". " << arr_opcion[i] << "\n";
         }
 
         cin>> opcion;
-        
 
         switch (opcion) {
             case 1:
-                opcion=0;
-            case 2:
-                
-                cout<<"Radi?\n";
-                cin>>area;
-                
-                //Creamos el objeto 
-                Circle circulo(area);
-                
-                cout<<"El area del circulo es "<<circulo.getArea()<<"\n";
+                opcion = 0;
                 break;
-       
+            case 2:
+                agregarFigura(contador);
+                break;
+
         }
 
     } while (opcion != 0);
     cout << "***FIN***\n";
 
     return 0;
+
+
 }
